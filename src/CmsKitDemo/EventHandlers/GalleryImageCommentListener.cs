37fe7b5c1d1439c549c270f6fc5c28f35c6134e2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2025-04-24 13:32:15 +0300

    Summarize comments

---
+++ src/CmsKitDemo/EventHandlers/GalleryImageCommentListener.cs
@@ -0,0 +1,70 @@
+﻿using CmsKitDemo.Entities;
+using CmsKitDemo.Utils;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Entities.Events;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.EventBus;
+using Volo.CmsKit.Comments;
+
+namespace CmsKitDemo.EventHandlers;
+
+public class GalleryImageCommentListener : ILocalEventHandler<EntityChangedEventData<Comment>>, ITransientDependency
+{
+    private readonly IRepository<GalleryImage, Guid> _galleryImageRepository;
+    private readonly IRepository<Comment, Guid> _commentRepository;
+    private readonly AiCommentSummarizer _aiCommentSummarizer;
+
+    public GalleryImageCommentListener(
+        IRepository<GalleryImage, Guid> galleryImageRepository,
+        IRepository<Comment, Guid> commentRepository,
+        AiCommentSummarizer aiCommentSummarizer)
+    {
+        _galleryImageRepository = galleryImageRepository;
+        _commentRepository = commentRepository;
+        _aiCommentSummarizer = aiCommentSummarizer;
+    }
+    
+    public async Task HandleEventAsync(EntityChangedEventData<Comment> eventData)
+    {
+        var comment = eventData.Entity;
+        
+        //Here, we only interest in comments related to image gallery items
+        if (comment.EntityType != CmsKitDemoConsts.ImageGalleryEntityType)
+        {
+            return;
+        }
+
+        if (!Guid.TryParse(comment.EntityId, out var galleryImageId))
+        {
+            return;
+        }
+        
+        // Get the related image from database
+        var galleryImage = await _galleryImageRepository.FindAsync(galleryImageId);
+        if (galleryImage == null)
+        {
+            return;
+        }
+        
+        // Get all the comments related to the image
+        var queryable = await _commentRepository.GetQueryableAsync();
+        var allCommentTexts = await queryable
+            .Where(c => c.EntityType == CmsKitDemoConsts.ImageGalleryEntityType && c.EntityId == comment.EntityId)
+            .Select(c => c.Text)
+            .ToArrayAsync();
+
+        // Update the summary of comments related to the image
+        if (allCommentTexts.Length <= 0)
+        {
+            galleryImage.CommentsSummary = "";
+        }
+        else
+        {
+            galleryImage.CommentsSummary = await _aiCommentSummarizer.SummarizeAsync(allCommentTexts);
+        }
+
+        // Update the image in database
+        await _galleryImageRepository.UpdateAsync(galleryImage);
+    }
+}
---
+++ src/CmsKitDemo/Utils/AiCommentSummarizer.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using Volo.Abp.DependencyInjection;
+
+namespace CmsKitDemo.Utils;
+
+public class AiCommentSummarizer : ITransientDependency
+{
+    private readonly IConfiguration _configuration;
+
+    public AiCommentSummarizer(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public async Task<string> SummarizeAsync(string[] commentTexts)
+    {
+        // Get the model and key from the configuration
+        var aiModel = _configuration["ModelName"];
+        var apiKey = _configuration["OpenAIKey"];
+
+        // Create the IChatClient
+        var client = new OpenAIClient(apiKey)
+            .GetChatClient(aiModel)
+            .AsIChatClient();
+
+        var promptBuilder = new StringBuilder();
+
+        promptBuilder.AppendLine(
+            @"There are comments from different users of our website about an image.
+We want to summarize the comments into a single comment.
+Return a single comment with a maximum of 512 characters. Comments are separated by a newline character and given below."
+        );
+        promptBuilder.AppendLine();
+        
+        foreach (var commentText in commentTexts)
+        {
+            promptBuilder.AppendLine("User comment:");
+            promptBuilder.AppendLine(commentText);
+            promptBuilder.AppendLine();
+        }
+        
+        // Submit the prompt and print out the response
+        var response = await client.GetResponseAsync(
+            promptBuilder.ToString(),
+            new ChatOptions { MaxOutputTokens = 1024 }
+        );
+
+        return response.Text;
+    }
+}
